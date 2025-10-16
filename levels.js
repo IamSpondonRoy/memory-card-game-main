25fb4b5fcd6376c180a8e630f1a9053765fc1c62
TerrenceGunawan <135194407+TerrenceGunawan@users.noreply.github.com>
2025-10-16 13:37:37 +1100

    Added easier level 1

--- levels.js
+++ levels.js
@@ -1,5 +1,6 @@
 // levels.js
 const LEVELS = [
+  { pairs: 3, rows: 2 },
   { pairs: 5, rows: 2 }, // Level 1 → 10 cards
   { pairs: 8, rows: 4 }, // Level 2 → 16 cards
   { pairs: 10, rows: 4 }, // Level 3 → 20 cards
